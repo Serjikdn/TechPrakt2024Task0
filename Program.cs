e308865d9c75ccbd338fd90a1627446a7ae1c020
Serjikdn <serjik.dn@gmail.com>
2024-02-08 16:39:14 +0200

    created new project C#

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number one: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number two: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Result: " + a + " + " + b + " = " + (a + b));
